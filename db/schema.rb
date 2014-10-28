05ceb70b5539909d54a561577672d9a47d9c7d74
mitchs <michel.seguin@gmail.com>
2014-10-28 04:35:31 +0000

    got microposts working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027161430) do
+ActiveRecord::Schema.define(version: 20141028035445) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",       limit: 255
